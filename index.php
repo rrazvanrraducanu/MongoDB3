526e8f04370ca0baa302c41c3056e6d51e544f66
Razvan Raducanu <rrazvan.rraducanu@gmail.com>
2018-04-03 13:04:32 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+       require_once 'connection.php';
+        $query = new MongoDB\Driver\Query([]); 
+        $rows = $client->executeQuery("flori.flowers", $query);
+?>
+<table>
+    <tr>
+        <td>Nume</td>
+        <td>Culoare</td>
+        <td>Marime</td>
+        <td>Pret</td>
+</tr>
+<?php foreach($rows as $val):?> 
+<?php if((isset($val->nume))&&(isset($val->culoare))&& (isset($val->marime))&&(isset($val->pret))&&($val->nume!="")&& ($val->culoare!="")):?>    
+<tr>
+    <td><?php echo $val->nume;?></td>
+    <td><?php echo $val->culoare;?></td>
+    <td><?php echo $val->marime;?></td>
+    <td><?php echo $val->pret;?></td>
+      <td><?php echo "<a href=\"view.php?id=".$val->_id."\">View</a>";?></td>
+</tr>
+    <?php endif;?>
+    <?php endforeach;?>
+</table>
+        <a href="insert.php">Add a new record</a><br><br>        
+</body>
+</html>
